72bdec17411c71777588e0dfa6524efe2abdfec2
LiuXiYing <2671642981@qq.com>
2024-04-23 22:36:05 +0800

    liu

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,22 @@
-// import { Detail } from "@raycast/api";
-// import { fetchData } from "./request";
-// export default function Command() {
-//     var data = fetchData('http://222.19.236.142:7070/info2')
-//     return <Detail markdown={{data}} />
-// }
 
 import { useEffect, useState } from "react";
 import { Detail } from "@raycast/api";
 import { fetchData } from "./request";
-import { json } from "stream/consumers";
-// import {}
+import { parseComputerInfo,ComputerInfo } from "./info";
 
 export default function Command() {
     var [data, setData] = useState<string>("");
-// var data = "sadsd"
 
     useEffect(() => {
         const fetchDataAsync = async () => {
             try {
-                const response = await fetchData('http://222.19.236.142:7070/info2');
-                setData(JSON.stringify(response));
-                console.log(JSON.stringify(response));
+                const response = await fetchData();
+                const computers: ComputerInfo[] = parseComputerInfo(JSON.parse(JSON.stringify(response)))
+                setData(JSON.stringify(computers));
             } catch (error) {
-                console.error('Error fetching data:', error);
+                setData(JSON.stringify(error));
             }
         };
-
         fetchDataAsync();
     }, []);
 
--- src/request.ts
+++ src/request.ts
@@ -7,16 +7,18 @@ interface Data {
 }
 
 // 定义一个异步函数来获取 JSON 数据
-export async function fetchData(url: string): Promise<Data> {
+export async function fetchData(): Promise<Data> {
     try {
         // 发起网络请求
+        var url = 'http://222.19.236.142:7070/info2'
         const response = await axios.get<Data>(url);
-
+        
         // 返回获取到的 JSON 数据
         return  JSON.stringify(response.data);
     } catch (error) {
         // 处理异常情况
-        console.error('There was a problem with the fetch operation:', error.message);
-        throw error;
+
+        var msg = '去找过文彬了，他说这个问题不是他的问题，是你的问题，你去找他吧！'
+        throw msg;
     }
 }
